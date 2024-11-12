de650499d1e899d7c5091be3fa1d29f910dec525
KoDongUk <bogo1357@hanmail.net>
2024-11-13 03:17:24 +0900

    feature: StackNavigation ResultScreen 스키린 추가

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -14,6 +14,7 @@ const StackNavigator = () => {
       <Stack.Screen name="ParticipantsSetScreen" component={ParticipantsSetScreen} />
       <Stack.Screen name="PrizeSetScreen" component={PrizeSetScreen} />
       <Stack.Screen name="DrawALotScreen" component={DrawALotScreen} />
+      <Stack.Screen name="ResultScreen" component={ResultScreen} />
     </Stack.Navigator>
   )
 }
